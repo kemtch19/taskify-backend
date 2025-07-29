d7c5dcc77961cd5d2f74c2bc4b0b3484a2f80813
kemtch <kemtch19@gmail.com>
2025-07-29 10:49:45 -0500

    fix: update SameSite attribute for cookies to 'Lax' for better compatibility

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,33 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const {
+  registerUser,
+  getUserById,
+  loginUser,
+  changePassword,
+  getCurrentUser,
+  updateProfileImage,
+  logoutUser,
+} = require("../controllers/usersController");
 // import validadores
-const { registerUserValidator } = require('../middlewares/validators/user/registerValidator');
-const { loginUserValidator } = require('../middlewares/validators/user/loginValidator');
-const validateResult = require('../middlewares/validators/errors/validateResult');
-const authMiddleware = require('../middlewares/authMiddleware');
-const uploadMiddleware = require('../middlewares/uploadMiddleware');
-const { changePasswordValidator } = require('../middlewares/validators/user/usersValidator');
+const {
+  registerUserValidator,
+} = require("../middlewares/validators/user/registerValidator");
+const {
+  loginUserValidator,
+} = require("../middlewares/validators/user/loginValidator");
+const validateResult = require("../middlewares/validators/errors/validateResult");
+const authMiddleware = require("../middlewares/authMiddleware");
+const uploadMiddleware = require("../middlewares/uploadMiddleware");
+const {
+  changePasswordValidator,
+} = require("../middlewares/validators/user/usersValidator");
 
 // ruta para ver el usuario actual
-router.get('/me', authMiddleware, getCurrentUser);
+router.get("/me", authMiddleware, getCurrentUser);
 
 //ruta para login
-router.post('/login', loginUserValidator, validateResult, loginUser);
+router.post("/login", loginUserValidator, validateResult, loginUser);
 
 // ruta para logout
-router.post('/logout', authMiddleware, logoutUser);
+router.post("/logout", authMiddleware, logoutUser);
 
 // ruta register, ya en app.js definimos la ruta para todos los archivos que tengamos aquí, y por ende todas las rutas de users van aquí, ya que en app.js se define la ruta de /api/user en general y ya acá se la damos en especifico /api/user/register
-router.post('/register', registerUserValidator, validateResult, registerUser);
+router.post("/register", registerUserValidator, validateResult, registerUser);
 
 // ruta para cambiar contraseña
-router.patch('/change-password', authMiddleware, changePasswordValidator, validateResult, changePassword);
+router.patch(
+  "/change-password",
+  authMiddleware,
+  changePasswordValidator,
+  validateResult,
+  changePassword
+);
 
 // ruta para actualizar la imagen del usuario
-router.post('/update-profile-image', authMiddleware, uploadMiddleware.single('image'), updateProfileImage);
-
+router.post(
+  "/update-profile-image",
+  authMiddleware,
+  uploadMiddleware.single("image"),
+  updateProfileImage
+);
 
 //ruta para ver todos los usuarios
-router.get('/:id', getUserById);
+router.get("/:id", getUserById);
 
 module.exports = router;
