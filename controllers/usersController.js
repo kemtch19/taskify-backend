eb46e66754f3cd40315be5ace279177b8a5e9a21
kemtch <kemtch19@gmail.com>
2025-08-04 16:34:05 -0500

    chore: change of secure from production with http cokies

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -32,7 +32,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // solo en HTTPS en prod
-      sameSite: "Lax", // o 'Lax' según tu configuración
+      sameSite: "Strict", // o 'Lax' según tu configuración
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
     });
 
